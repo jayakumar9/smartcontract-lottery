f460b7e4268b0733fb155939a2ff2b4f5afa1cbd
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-01 17:08:41 +0530

    Update Lottery.sol
    save

--- Lottery.sol
+++ Lottery.sol
@@ -1,32 +1,45 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";//copied fromhttps://docs.openzeppelin.com/contracts/4.x/api/access#Ownable
 
 contract Lottery {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE{
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
     
     constructor(address _priceFeedAddress) public{
         usdEntryFee=50*(10**18);
         ethUsdPriceFeed=AggregatorV3Interface(_priceFeedAddress)
+        lottery_state=LOTTERY_STATE.CLOSED;
      }   
 
     function enter() public{} payable {
     //$50 minimum
-    require();
+    require(lottery_state)==LOTTERY_STATE.OPEN();
+    require(msg.value >=getEntranceFee(),"Not Enough ETH!");    
     players.push(msg.sender);
     }
     function getEntranceFee() public view returns(uint256){
-        (,int256 price, , , , )=ethUsdPriceFeed.latestRoundData;
+        (,int256 price, , , )=ethUsdPriceFeed.latestRoundData;
         uint256 adjustedPrice=uint256(price)*10**10;//18 decimals
         //$50,$2000/ETH
         //50/2,000
         //50*100000/2000
-        uint256 costToEnter=(usdEntryFee *10**18)/price;
+        uint256 costToEnter=(usdEntryFee *10**18)/adjustedPrice;
         return costToEnter;
     }
-    function startLottery() public{}
+    function startLottery() public onlyOwner{
+        require(lottery_state==LOTTERY_STATE.CLOSED,"can't start a new lottery yet!");
+        lottery_state==LOTTERY_STATE.OPEN;
+        
+    }
     function endLottery() public{}
     
 
