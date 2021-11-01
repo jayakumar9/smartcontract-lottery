5ef7a074a5036e27513fac7bc2ad92ed94a5f146
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-01 16:46:46 +0530

    Update test_lottery.py
    save

--- test_lottery.py
+++ test_lottery.py
@@ -2,9 +2,12 @@
 # $50 minimum price lottery so, 50/2598.29=0.019243425
 # 190000000000000000
 
-from brownie import Lottery, accounts
+from brownie import Lottery, accounts, confiq
+from web3 import web3
 
 test_get_entrance_fee():
   account=accounts[0]
-  lottery=Lottery.deploy(????,{"from":account})
+  lottery=Lottery.deploy(confiq["networks"][network.show_active()]["eth_usd_price_feed"],{"from":account})
+  assert lottery.getEntranceFee()>web3.toWei(0.018,"ether")
+   assert lottery.getEntranceFee()<web3.toWei(0.022,"ether")
   
