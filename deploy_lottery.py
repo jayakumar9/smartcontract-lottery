c8585928043c9bdeb2b9eaf0dcdb0957cc6bd7d4
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-02 17:04:07 +0530

    Update deploy_lottery.py
    save

--- deploy_lottery.py
+++ deploy_lottery.py
@@ -7,7 +7,8 @@ def deploy_lottery():
     account=get_account()
     lottery=Lottery.deploy(get_contract("eth_usd_price_feed").address,get_contract("vrf_coordinator").address,get_contract("link_token".address,
                            config["networks"][network.show_active()]["fee"],config["networks"][network.show_active()]["keyhash"],{"from":account},
-                            publish_source=config["networks"][network.show_active().get("verify",False),])
+                            publish_source=config["networks"][network.show_active().get("verify",False),)
+    print("Deployed lottery!")                                                         
     
 
 def main()
