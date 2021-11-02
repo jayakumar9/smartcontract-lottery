2a81c395ddb37bf3e68a0f14d66aad9744891e46
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-02 20:31:16 +0530

    saved at video 3.solidity time 0:17:03

--- helpful_scripts.py
+++ helpful_scripts.py
@@ -60,13 +60,14 @@ INITIAL_VALUE=200000000000
     print("Deployed!")
 
     
-def fund_with_link(contract_address, account=None, link_token=None, amount=100000000000000000 ) #0.1L    
+def fund_with_link(contract_address, account=None, link_token=None, amount=100000000000000000 ) #0.1Link   
     account= account if account else get_account()
     link_token= link_token if link_token else get_contract("link_token")
-     #tx=link_token.transfer(contract_address, amount, {"from":account})
+    tx=link_token.transfer(contract_address, amount, {"from":account})
     # link_token_contract=interface.LinkTokenInterface(link_token.address)
     # tx=link_token_contract.transfer(contract_address,amount,{"from":account})
-    link_token_contract=interface.LinkTokenInterface(link_token.address)
+    #link_token_contract=interface.LinkTokenInterface(link_token.address)
+    #tx=link_token_contract.transfer(contract_address, amount,{"from":account})
     tx.wait(1)
     print("Fund contract!")
     return tx
