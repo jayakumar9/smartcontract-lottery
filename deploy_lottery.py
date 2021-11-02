efbfd346a25d9404b984c3400f482b86ad63405c
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-02 17:17:06 +0530

    saved  at video 3.solidity time 0"08:33

--- deploy_lottery.py
+++ deploy_lottery.py
@@ -8,8 +8,15 @@ def deploy_lottery():
     lottery=Lottery.deploy(get_contract("eth_usd_price_feed").address,get_contract("vrf_coordinator").address,get_contract("link_token".address,
                            config["networks"][network.show_active()]["fee"],config["networks"][network.show_active()]["keyhash"],{"from":account},
                             publish_source=config["networks"][network.show_active().get("verify",False),)
-    print("Deployed lottery!")                                                         
-    
+    print("Deployed lottery!")
+                                                              
+                                                       
+                                                              
+def start_lottery():
+    account=get_account()
+    lottery=Lottery[-1]                                                              
+    lottery.startLottery({"from":account})
+    print("The lottery is started!")                                                              
 
 def main()
     deploy_lottery()
