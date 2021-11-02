2ab77b0b1f108056239b0e72eefa1c0cdddd3bc6
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-02 13:14:31 +0530

    Update helpful_scripts.py
    save at 2.solidity time 2:53:39

--- helpful_scripts.py
+++ helpful_scripts.py
@@ -17,8 +17,9 @@ def get_account(index=None,id=None):
         or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
       ):  
         return accounts[0]
-     else:
-        return accounts.add(config["wallets"]["from_key"])
+    return accounts.add(config["wallets"]["from_key"])
+
+contract_to_mock={"eth_usd_price_feed":MockV3Aggregator}
     
  def get_contract(contract_name):
     """ This function will grab the contract address from the brownie config 
@@ -32,4 +33,21 @@ def get_account(index=None,id=None):
         brownie.network.contract.projectContract:The most recently deployed version of this contract.    
     
     """
+    contract_type=contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type)<=0:
+            # MockV3Aggregator.length
+            deploy_mocks()
+        contract=contract_type[-1]  
+         # MockV3Aggregator[-1]
+    else:
+        contract_address=config["networks"][network.show_active()][contract_name]
+            
+DECIMALS=8
+INITIAL_VALUE=200000000000
+            
+ def deploy_mock(decimals=DECIMALS,initial_value=INITIAL_VALUE):
+    account=get_account()
+    mock_price_feed=MockV3Aggregator.deploy(decimals, initial_value, {"from":account})
+    print("Deployed!")
             
