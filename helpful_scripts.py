5c7d7667985be3ca7ddbbc5d619cf5968eba4266
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-02 07:59:29 +0530

    Update helpful_scripts.py
    save

--- helpful_scripts.py
+++ helpful_scripts.py
@@ -1,4 +1,15 @@
 # create this file under smartcontract-lottery/scripts refer video 2.solidity time 2:41:55
 
+from brownie import accounts, network, config
+FORKED_LOCAL_ENVIRONMENTS=["mainnet-fork","mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS=["development","ganache-local"]
+
 def get_account():
-    pass
+    if(
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+      ):  
+        RETURN ACCOUNTS[0]
+     else:
+        return accounts.add(config["wallets"]["from_key"])
+            
