ae3ad3caf403a2ac92407dee3b9c6e0b6a27698d
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-02 13:25:49 +0530

    Update deploy_lottery.py
    save

--- deploy_lottery.py
+++ deploy_lottery.py
@@ -1,11 +1,11 @@
 # create this file under smartcontract-lottery/scripts refer video 2.solidity time 2:40:42
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, get_contract
 from brownie import Lottery
 
 
 def deploy_lottery():
     account=get_account()
-    lottery=Lottery.deploy(
+    lottery=Lottery.deploy(get_contract("eth_usd_price_feed").address)
 
 def main()
     deploy_lottery()
