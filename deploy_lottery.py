6b02408e72f6d9c11f2c2164661ddb804aafc1c4
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-02 08:21:22 +0530

    Update deploy_lottery.py
    save

--- deploy_lottery.py
+++ deploy_lottery.py
@@ -1,8 +1,11 @@
 # create this file under smartcontract-lottery/scripts refer video 2.solidity time 2:40:42
+from scripts.helpful_scripts import get_account
+from brownie import Lottery
 
 
 def deploy_lottery():
     account=get_account()
+    lottery=Lottery.deploy(
 
 def main()
     deploy_lottery()
