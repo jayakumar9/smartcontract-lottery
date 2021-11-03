a8313244c9d3716736fb0cf5fa3598d59d58e143
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-03 07:58:07 +0530

    saved video 3.solidity time 0:23:38

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -4,12 +4,10 @@
 #0.019
 # 190000000000000000
 from brownie import Lottery. accounts. config, network
-from web3 import web3
+from scripts.deploy_lottery import deploy_lottery
 
 
 def test_get_entrance_fee():
-    account=accounts[0]
-    lottery=Lottery.deploy(config["network"][network.show_active()]["eth_usd_price_feed"],{"from":account},)
-    #assert lottery.getEntranceFee()>web3.towei(0.018,"ether")
-    #assert lottery.getEntranceFee()<web3.towei(0.022,"ether")
+    lottery=
+ 
 
