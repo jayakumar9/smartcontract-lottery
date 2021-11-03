b8180610b9eec42c6cb266f348caee5b19e53a71
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-03 07:23:12 +0530

    saved at time 0:22:59

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -1,1 +1,15 @@
 # create this file under smartcontract-lottery/tests
+
+
+#0.019
+# 190000000000000000
+from brownie import Lottery. accounts. config, network
+from web3 import web3
+
+
+def test_get_entrance_fee():
+    account=accounts[0]
+    lottery=Lottery.deploy(config["network"][network.show_active()]["eth_usd_price_feed"],{"from":account},)
+    #assert lottery.getEntranceFee()>web3.towei(0.018,"ether")
+    #assert lottery.getEntranceFee()<web3.towei(0.022,"ether")
+
