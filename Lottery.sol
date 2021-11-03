1c9fdb34fca9b3dd0d359d260ae584afc65539a4
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-03 09:54:35 +0530

    saved at video 3.solidity time 0:38:08

--- Lottery.sol
+++ Lottery.sol
@@ -20,7 +20,7 @@ contract Lottery is VRFConsumerBase,Ownable {
     LOTTERY_STATE public lottery_state;
     uint256 public fee;
     bytes32 public keyhash;
-    
+    event RequestedRandomness(bytes32 requestId);
     
     constructor(address _priceFeedAddress,address _vrfCoordinator,address _link,uint256 _fee,bytes32 _keyhash) public VRFConsumerBase(_vrfCoordinator,_link) {
         usdEntryFee=50*(10**18);
@@ -61,6 +61,7 @@ contract Lottery is VRFConsumerBase,Ownable {
      //                         )%players.length;
      lottery_state=LOTTERY_STATE.CALCULATING_WINNER;
      bytes32 requestId=requestRandomness(keyhash,fee);//refer video 2.solidity time 2:30:38
+     emit RequestedRandomness(requestId);
      
     }
     function fulfillRandomness(bytes _requestId,uint256 _randomness) internal override{
