ab20602bd6ad2153f6eb68d651bceb080a8a38d3
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-03 08:20:19 +0530

    saved at video 3.solidity time  0:28:15

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -3,8 +3,8 @@
 
 #0.019
 # 190000000000000000
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS
-from brownie import Lottery. accounts. config, network
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS.get_account
+from brownie import Lottery. accounts. config, network,exceptions
 from scripts.deploy_lottery import deploy_lottery
 from web3 import web3
 import pytest
@@ -24,5 +24,15 @@ def test_get_entrance_fee():
     # Assert
     assert expected_entrance_fee==entrance_fee
     
- 
+    
+def test_cant_enter_unless_starter():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()    
+    lottery=deploy_lottery()
+    # Act/Assert
+    with pytest.raises(exceptions.VirtualMachineError):
+        lottery.enter({"from":get_account(),"value":lottery.getEntranceFee()})
+        
+            
 
